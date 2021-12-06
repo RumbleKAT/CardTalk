8bfb005456e1e7c1285aaf224c3856541638979d
RumbleKAT <reki318@naver.com>
2021-12-06 23:27:52 +0900

    add test sample file

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -4,6 +4,10 @@ describe('Sum test',() => {
     test('two plus two is four', () => {
         expect(sum(2,2)).toBe(4);
     });
+    
+    test('two plus two is four', () => {
+        expect(sum(2,2)).toBe(5);
+    });
 })
 
 export{}
