ed42c39b728bcf8451fb9cfc79c3748c54ba7aeb
RumbleKAT <reki318@naver.com>
2021-12-08 23:06:39 +0900

    sub test

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,8 +1,11 @@
-import { sum } from "../utils/calculator";
+import {sum, sub} from "../utils/calculator";
 
 describe('Sum test',() => {
     test('two plus two is four', () => {
         expect(sum(2,2)).toBe(4);
     });
     
+    test('subtract 4 - 2  = 2', () => {
+        expect(sub(4,2)).toBe(1)
+    })
 })
